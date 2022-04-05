ec11a61d8f2e161b71a6109984ec90f5e4cb2cf7
K-Ds <kardav2003@gmail.com>
2022-04-05 09:59:43 +0200

    Fixing bugs

---
+++ UI/scripts/blogEdit.js
@@ -0,0 +1,19 @@
+import * as validator from "./verification.js";
+
+let newBlogForm = document.querySelector("#newBlogForm");
+let newBlogImg = newBlogForm.elements["blog-img"];
+let newBlogBody = newBlogForm.elements["blog-body"];
+let newBlogTitle = newBlogForm.elements["title"];
+
+newBlogForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let titleValid = validator.nameVerification(newBlogTitle, 45);
+  let bodyValid = validator.nameVerification(newBlogBody);
+
+  let imageValid = validator.nameVerification(newBlogImg);
+  console.log(newBlogImg.value);
+  if (titleValid && bodyValid && imageValid) {
+    postBlogData(newBlogTitle, newBlogBody, newBlogImg);
+    location.href = "admin.html";
+  }
+});
