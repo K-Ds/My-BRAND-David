abffcba1ec311ee2ea8589ede834a04fc3cb96af
K-Ds <kardav2003@gmail.com>
2022-04-05 08:46:13 +0200

    Bug: hide mavbar after click

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -2,11 +2,16 @@ let navMenu = document.querySelector(".nav__menu");
 let navList = document.querySelector(".nav__toggler");
 let navIcons = document.querySelectorAll(".nav__menu-icon");
 
-navMenu.addEventListener("click", hideMenu);
-
-function hideMenu() {
+navMenu.addEventListener("click", () => {
   navList.classList.toggle("open");
   navIcons.forEach((icon) => {
     icon.classList.toggle("hidden");
   });
-}
+});
+
+navList.addEventListener("click", () => {
+  navList.classList.remove("open");
+  navIcons.forEach((icon) => {
+    icon.classList.toggle("hidden");
+  });
+});
