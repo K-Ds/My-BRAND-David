68df941eb6eda875f40ad77c1406ae6c944bc220
K-Ds <68099724+K-Ds@users.noreply.github.com>
2022-04-19 05:38:33 +0200

    testing ci.

--- Server/src/controllers/postControllers.js
+++ Server/src/controllers/postControllers.js
@@ -29,7 +29,7 @@ export const newPost = async (req, res) => {
     const cloudinaryResult = await cloudinary(req.body.img);
     imgUrl = cloudinaryResult.url;
   } catch (err) {
-    return res.status(500).json({ error: err.message });
+    return res.status(501).json({ error: err.message });
   }
   try {
     const input = {
