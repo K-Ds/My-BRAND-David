750f3601b16b4da6abf499aa56c49a2728066841
K-Ds <68099724+K-Ds@users.noreply.github.com>
2022-04-20 15:42:03 +0200

    testing cloudinary.

--- Server/src/cloudinary.js
+++ Server/src/cloudinary.js
@@ -1,17 +1,17 @@
-import * as Cloudinary from "cloudinary";
+const cloudinary = require("cloudinary");
 
-Cloudinary.config({
+cloudinary.config({
   cloud_name: "k-ds",
   api_key: "823114164825559",
   api_secret: "cTaAd4Nmhx9HDZQkx6OsngbpT5M",
 });
 
-// const upload = async (image) => {
-//   const result = Cloudinary.uploader.upload(image).then((result) => {
-//     return result;
-//   });
-//   return result;
-// };
+const upload = async (image) => {
+  const result = cloudinary.uploader.upload_(image).then((result) => {
+    return result;
+  });
+  return result;
+};
 
 const upload_test = async (image) => {
   return {
@@ -19,12 +19,12 @@ const upload_test = async (image) => {
   };
 };
 
-// let default_export = upload;
+let default_export = upload;
 
-// if (process.env.NODE_ENV == "test") {
-//   default_export = upload_test;
-// }
+if (process.env.NODE_ENV == "test") {
+  default_export = upload_test;
+}
 
-// export default default_export;
+export default default_export;
 
-export default upload_test;
+// export default upload;
