0853ec6798aa2819fe565211ecc1c937b8651b31
K-Ds <68099724+K-Ds@users.noreply.github.com>
2022-04-20 09:17:56 +0200

    testing circleci

--- Server/src/cloudinary.js
+++ Server/src/cloudinary.js
@@ -3,7 +3,7 @@ import * as Cloudinary from "cloudinary";
 Cloudinary.config({
   cloud_name: "k-ds",
   api_key: 823114164825559,
-  api_secret: cTaAd4Nmhx9HDZQkx6OsngbpT5M,
+  api_secret: "cTaAd4Nmhx9HDZQkx6OsngbpT5M",
 });
 
 const upload = async (image) => {
